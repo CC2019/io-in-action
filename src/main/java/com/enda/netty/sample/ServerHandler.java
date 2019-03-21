f3e98f2252beb91853c9f917b4f3c5c32316c3a4
wt <1139914806@qq.com>
2019-03-21 15:30:51 +0800

    sample

---
+++ src/main/java/com/enda/netty/sample/ServerHandler.java
@@ -0,0 +1,26 @@
+package com.enda.netty.sample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+/**
+ * @author wt
+ * @create 2019-03-21 上午9:45
+ */
+public class ServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
+        System.out.println(channelHandlerContext.channel().remoteAddress() + "," + msg);
+        channelHandlerContext.channel().writeAndFlush("from server: " + UUID.randomUUID());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+        super.exceptionCaught(ctx, cause);
+    }
+}
---
+++ src/main/java/com/enda/netty/sample/ServerInitializer.java
@@ -0,0 +1,34 @@
+package com.enda.netty.sample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+
+/**
+ * @author wt
+ * @create 2019-03-20 上午9:52
+ */
+public class ServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+
+//        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+//        pipeline.addLast("HttpServerHandler", new HttpServerHandler());
+
+        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+        pipeline.addLast(new LengthFieldPrepender(4));
+        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+        pipeline.addLast("myHandler", new ServerHandler());
+    }
+
+
+}
