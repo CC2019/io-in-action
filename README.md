02278ad0816b7203648c1bee0a0ca348c4e590a6
wt <1139914806@qq.com>
2019-03-26 10:07:02 +0800

    update something

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # io-in-action
 JAVA IO, NIO, Netty, 多线程并发实战
+
+
+#### 基于Netty实现WS协议
+![](images/ws.png)
---
+++ src/main/java/com/enda/Main.java
@@ -0,0 +1,17 @@
+package com.enda;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        System.out.println((1+n)*n/2);
+    }
+}
